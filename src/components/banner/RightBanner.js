175c3536660dd5b524920ca8d19ee45e8fcc35d6
Naveen Vijayakumar <naveen26898@outlook.com>
2023-09-14 03:03:26 +0000

    testimonial edit

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -6,7 +6,7 @@ const RightBanner = () => {
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
       <img
         className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
-        //src={bannerImg}
+        src={bannerImg}
         alt="bannerImg"
       />
       <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
--- src/components/tesimonial/Testimonial.js
+++ src/components/tesimonial/Testimonial.js
@@ -6,6 +6,7 @@ import Title from '../layouts/Title'
 import { testimonialOne,testimonialTwo,testimonialThree, quote } from "../../assets";
 
 
+
 function SampleNextArrow(props) {
   const { onClick } = props;
   return (
@@ -99,50 +100,31 @@ const Testimonial = () => {
         {/* ================ Slider One ================== */}
         <Slider {...settings}>
           <div className="w-full">
-            <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
-              <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
-                <img
-                  className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
-                  src={testimonialOne}
-                  alt="testimonialOne"
-                />
+            
+               
                 <div className="w-full flex flex-col justify-end">
-                  <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    Spider Man
-                  </p>
-                  <h3 className="text-2xl font-bold">Peter Parker</h3>
-                  <p className="text-base tracking-wide text-gray-500">
-                    Sr. Web Developer
-                  </p>
-                </div>
-              </div>
+                  
+                  
+                 
+             
               <div className="w-full lgl:w-[60%] h-full flex flex-col justify-between">
-                <img className="w-20 lgl:w-32" src={quote} alt="quote" />
-                <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
-                  <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
+        
+                
+                  
                     <div>
                       <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
                         After School Diaries 
                       </h3>
-                      <p className="text-base text-gray-400 mt-3" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        via Upwork - March 2023
-                      </p>
                     </div>
-                    <div className="text-yellow-500 flex gap-1">
-                      <RiStarFill />
-                      <RiStarFill />
-                      <RiStarFill />
-                      <RiStarFill />
-                      <RiStarFill />
-                    </div>
+                    
                   </div>
                   <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
                     Very good concept of bundling all the old cartoons and shows' songs together on one page. It is quite nostalgic for the Gen Z. The slide effect is amazing and UI is smooth. Very good project !
                   </p>
                 </div>
               </div>
-            </div>
-          </div>
+            
+          
           {/* ================ Slider Two ================== */}
 
           <div className="w-full">
@@ -231,8 +213,8 @@ const Testimonial = () => {
                     </div>
                   </div>
                   <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
-                    Good concept of sharing stories and secrets without the fear of anyone judging you or Google tracking you to bombard related advertisements (😂).
-                  </p>
+                  Hi Shweta, Sanjana sang beautifully yesterday with a steady voice and calm demeanor. The 2 and half hours was mesmerising. You and Jagadeesh must be very proud of her. Yesterday you were busy and couldn't pass my message. Thank you for inviting us. Selvi💕
+                   </p>
                 </div>
               </div>
             </div>
