d6b3b0fc60234343bdbef636ef9d860d974919b0
Naveen Vijayakumar <naveen26898@outlook.com>
2023-09-20 22:56:31 +0000

    New Commit

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 import { AiFillAppstore } from "react-icons/ai";
 import {  FaBrain} from "react-icons/fa";
-import { SiProgress} from "react-icons/si";
+import { BsMusicNoteList} from "react-icons/bs";
 import { CgWebsite } from "react-icons/cg";
 import { MdAnalytics } from "react-icons/md";
 
@@ -8,18 +8,19 @@ import { MdAnalytics } from "react-icons/md";
 export const featuresData = [
   {
     id: 1,
-    title: "Project Management",
-    des: "Want to gain real-life experience of managing a complete software project cycle.",
+    icon: <BsMusicNoteList />,
+    title: "Carnatic Music",
+    des: "Carnatic Music is a form of Indian classical music with origins in Southern India. Lyrics in Carnatic music are largely devotional; most of the songs are addressed to the Hindu deities. Many songs emphasize love and other social issues or rest on the concept of sublimation of human emotions for union with the divine.",
   },
   {
     id: 2,
     icon: <AiFillAppstore />,
-    title: "App Development",
-    des: "Will learn this soon in near future !",
+    title: "Bharatanatyam",
+    des: "Bharatanatyam is a dance of Tamil Nadu in southern India. It traces its origins back to the Natyashastra, an ancient treatise on theatre written by the mythic priest Bharata. Originally a temple dance for women, bharatanatyam often is used to express Hindu religious stories and devotions.",
   },
   {
     id: 3,
-    icon: <SiProgress />,
+    icon: <BsMusicNoteList />,
     title: "SEO Optimisation",
     des: "Will learn this soon in near future !",
   },
