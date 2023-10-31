ad34d67e148e58bbceebf31e9c68b4da52df756c
Naveen Vijayakumar <naveen26898@outlook.com>
2023-10-31 01:24:24 +0000

    1.0

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -13,7 +13,7 @@ const LeftBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
-        <h4 className=" text-lg font-normal">Hola Amingos 👀🙋‍♂️  </h4>
+        <h4 className=" text-lg font-normal">Hola Amigos 👀🙋‍♂️  </h4>
         <h1 className="text-6xl font-bold text-white">
           Hi, I'm <span className="text-designColor capitalize">Sanjana Devarajan</span>
         </h1>
